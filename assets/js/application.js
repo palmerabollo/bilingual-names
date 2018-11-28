34d325c2dfa1130b604443d64510df0b733367ba
Guido García <guido.garciabernardo@telefonica.com>
2018-11-28 17:34:04 +0100

    chore: improve ui

--- assets/js/application.js
+++ assets/js/application.js
@@ -13,18 +13,22 @@
       var lang1 = $("#first-language").val();
       var lang2 = $("#second-language").val();
 
-      if (lang1 && lang2 && lang1 != lang2) {
-        var items1 = names[lang1];
-        var items2 = names[lang2];
-
-        var results = _.intersectionBy(items1, items2, 'name');
-
-        if (results.length === 0) {
-          $("#results").html("No results found");
+      if (lang1 && lang2) {
+        if (lang1 === lang2) {
+          $("#results").html("No results found. Please, select two different languages.");
         } else {
-          results = _.sortBy(results, 'name');
-          for (var i = 0; i < results.length; i++) {
-            $("#results").append('<span><a href="http://www.bounty.com/pregnancy-and-birth/baby-names/baby-name-search/' + results[i].name[0] + '/' + results[i].name + '">' + results[i].name + '</a></span> · ');
+          var items1 = names[lang1];
+          var items2 = names[lang2];
+
+          var results = _.intersectionBy(items1, items2, 'name');
+
+          if (results.length === 0) {
+            $("#results").html("No results found :(");
+          } else {
+            results = _.sortBy(results, 'name');
+            for (var i = 0; i < results.length; i++) {
+              $("#results").append('<span><a href="http://www.bounty.com/pregnancy-and-birth/baby-names/baby-name-search/' + results[i].name[0] + '/' + results[i].name + '">' + results[i].name + '</a></span> · ');
+            }
           }
         }
       }
